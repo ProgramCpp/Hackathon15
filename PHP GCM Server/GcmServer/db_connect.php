0b0dcf88e8148712a0a2ed7d0f68c7459fbe7081
ProgramCpp <janani.anbarasan@gmail.com>
2015-03-07 12:40:30 +0530

    insert and query CRUD operations
    connect and query with PDO.

---
+++ PHP GCM Server/GcmServer/db_connect.php
@@ -0,0 +1,28 @@
+﻿<?php
+  
+class DB_Connect {
+  
+    // constructor
+    function __construct() {
+  
+    }
+  
+    // destructor
+    function __destruct() {
+        // $this->close();
+    }
+  
+    // Connecting to database
+    public function connect() {
+	$db = new PDO('mysql:host=' + DB_HOST +';dbname=' + DB_DATABASE + ';charset=utf8', DB_USER, DB_PASSWORD);
+        // return database handler
+        return $db;
+    }
+  
+    // Closing database connection
+    public function close() {
+        mysql_close();
+    }
+  
+} 
+?>
---
+++ PHP GCM Server/GcmServer/db_functions.php
@@ -0,0 +1,68 @@
+﻿<?php
+ 
+class DB_Functions {
+ 
+    private $db;
+ 
+    //put your code here
+    // constructor
+    function __construct() {
+        include_once './db_connect.php';
+        // connecting to database
+	$this->db = new PDO('mysql:host=localhost;dbname=winappgcm;charset=utf8', 'root');
+	$this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	$this->db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+    }
+ 
+    // destructor
+    function __destruct() {
+         
+    }
+ 
+    /**
+     * Storing new user
+     * returns user details
+     */
+    public function storeUser($name, $email, $gcm_regid) {
+        // insert user into database
+	try {
+        $result = $db->exec("INSERT INTO gcm_users(name, email, gcm_regid, created_at) VALUES('$name', '$email', '$gcm_regid', NOW())");
+        // check for successful store
+	if ($result) 
+	{
+            // get user details
+            $id = $db->lastInsertId();
+            $stmt = $db->query("SELECT * FROM gcm_users WHERE id = $id");
+            // return user details
+		if ($stmt->rowCount() > 0) 
+		{
+			$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        	return $results;
+		}
+		else
+		{
+			return false;
+		}
+    } 
+	else
+	{
+         return false;
+    }
+	
+    }
+	catch(PDOException $ex){
+            return false;
+        }
+    }
+ 
+    /**
+     * Getting all users
+     */
+    public function getAllUsers() {
+	$stmt = $this->db->query("select * FROM gcm_users");
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+    
+}
+ 
+?>
