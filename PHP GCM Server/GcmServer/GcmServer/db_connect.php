acbee99b8d76a233975bbacb88b9b33b3d52c5ee
ProgramCpp <janani.anbarasan@gmail.com>
2015-03-09 21:50:57 +0530

    php files
    server commit

---
+++ PHP GCM Server/GcmServer/GcmServer/db_connect.php
@@ -0,0 +1,30 @@
+﻿<?php
+  
+class DB_Connect {
+  
+    // constructor
+    function __construct() {
+  
+    }
+  
+    // destructor
+    function __destruct() {
+        // $this->close();
+    }
+  
+    // Connecting to database
+    public function connect() {
+	$db = new PDO('mysql:host=localhost;dbname=winappgcm;charset=utf8', 'root');
+	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	$db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+        // return database handler
+        return $db;
+    }
+  
+    // Closing database connection
+    public function close() {
+        /*mysql_close();*/
+    }
+  
+} 
+?>
---
+++ PHP GCM Server/GcmServer/GcmServer/register.php
@@ -0,0 +1,34 @@
+﻿<?php
+
+// response json
+$json = array();
+ 
+/**
+ * Registering a user device
+ * Store reg id in users table
+ */
+error_log("received", 0);
+if (isset($_POST["name"]) && isset($_POST["email"]) && isset($_POST["regId"])) {
+    $name = $_POST["name"];
+error_log("$name", 0);
+    $email = $_POST["email"];
+    $gcm_regid = $_POST["regId"]; // GCM Registration ID
+    // Store user details in db
+    include_once './db_functions.php';
+    include_once './GCM.php';
+ 
+    $db = new DB_Functions();
+    $gcm = new GCM();
+    $res = $db->storeUser($name, $email, $gcm_regid);
+$no_of_users =count($res);
+
+    $registatoin_ids = array($gcm_regid);
+    $message = array("product" => "shirt");
+ 
+    $result = $gcm->send_notification($registatoin_ids, $message);
+ 
+    echo $result;
+} else {
+    // user details missing
+}
+?>
